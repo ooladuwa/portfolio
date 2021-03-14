f6e844556b559ba1028af9989f3b2b85b8b0a3a3
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-03-13 23:28:39 -0500

    landing page

--- src/App.js
+++ src/App.js
@@ -19,8 +19,50 @@ function App() {
           </div>
         </nav>
       </header>
-      {/* <h1>Onaje Oladuwa</h1> */}
-      <div className="seedlings"></div>
+      <h1>Onaje Oladuwa</h1>
+      <h3>Full-Stack Developer</h3>
+      <div className="seedlings">
+        <img
+          src="https://i.ibb.co/n0jnPH1/seedlings.png"
+          alt="seedlings"
+          usemap="#image-map"
+        />
+        <map name="image-map">
+          <area
+            target="_blank"
+            alt="To Do"
+            title="To Do"
+            href="https://www.twitter.com"
+            coords="714,667,76"
+            shape="circle"
+          />
+
+          {/* <area
+            target="_blank"
+            alt="Shopping Cart"
+            title="Shopping Cart"
+            href="https://www.facebook.com"
+            coords="694,849,54"
+            shape="circle"
+          />
+          <area
+            target="_blank"
+            alt="Instagram Clone"
+            title="Instagram Clone"
+            href="https://www.instagram.com"
+            coords="1385,1149,89"
+            shape="circle"
+          />
+          <area
+            target="_blank"
+            alt="Resume"
+            title="Resume"
+            href="https://www.amazon.com"
+            coords="1282,637,62"
+            shape="circle"
+          /> */}
+        </map>
+      </div>
     </div>
   );
 }
